d9cd8bffe8ca16de75cf665ca32e040d7a6310f5
Alizamankhan786 <az030366@gmail.com>
2024-05-20 12:46:39 -0700

    index file created

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const image = document.querySelector(`#image`);
+
+image.addEventListener(`mouseover` , function(){
+    image.src="./bulb on.png" 
+
+})
+
+image.addEventListener(`mouseout` , function(){
+    image.src="./bulb off.jpeg"
+
+})
